542d277cfa9a47eb6df76eb38cfdce307a156005
Facundo Canteros <facundocanteros1995@gmail.com>
2024-06-11 15:53:38 -0300

    Metodo constructor. Accedo a sus propiedades

--- index.php
+++ index.php
@@ -3,5 +3,23 @@
 class Empleado{
     private $nombre;
     private $salario;
+
+
+    //Constructor
+    public function __construct($nombre,$salario)
+    {
+        $this->nombre=$nombre;
+        $this->salario=$salario;
+    }
+
+    //Accediendo a las propiedades
+
+    public function getNombre(){
+        return $this->nombre;  //Devuelve el valor nombre
+    }
+
+    public function getSalario(){
+        return $this->salario; //Devuelve el valor salario
+    }
 };
 ?>
